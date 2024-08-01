5aa344c974a184389071a31c28fcc55101b10ba8
sstobinski <60538960+sstobinski@users.noreply.github.com>
2024-08-01 09:08:15 +0200

    Add files via upload

---
+++ MemoryLeakTest/MyModels/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace MemoryLeakTest.MyModels;
+
+public class Car
+{
+    public string Brand { get; set; }
+
+    public Car(string brand)
+    {
+        Brand = brand;
+    }
+}
